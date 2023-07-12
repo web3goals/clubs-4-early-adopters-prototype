496e7dac0a3c316c0c7d3f91847a9a419629a08d
kiv1n <aloha@kiv1n.ru>
2023-07-12 19:02:53 +0300

    Fix imports for contracts project

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -4,7 +4,7 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const lockedAmount = ethers.utils.parseEther("0.001");
 
   const lock = await ethers.deployContract("Lock", [unlockTime], {
     value: lockedAmount,
@@ -13,7 +13,7 @@ async function main() {
   await lock.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
+    `Lock with ${ethers.utils.formatEther(
       lockedAmount
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
   );
--- contracts/test/Lock.ts
+++ contracts/test/Lock.ts
@@ -1,8 +1,5 @@
-import {
-  time,
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
